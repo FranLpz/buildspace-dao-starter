302b394d3f365905e39bf6da634b0f6b3f7c46a0
FranLpz <franciscoalopezd@gmail.com>
2021-12-24 19:41:39 -0500

    Handle basic unsupported network error.

--- src/App.jsx
+++ src/App.jsx
@@ -181,6 +181,19 @@ const App = () => {
       });
   }, [address]);
 
+  //Handle basic unsupported network error.
+  if (error && error.name === "UnsupportedChainIdError") {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
+
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
